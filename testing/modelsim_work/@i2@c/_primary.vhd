fcbccdaa7600f2783341c84c0e7c6639ddf60a01
Joshua Stevens <jasteve4@ncsu.edu>
2013-12-27 15:00:13 -0500

    Updates to I2C.v

--- testing/modelsim_work/@i2@c/_primary.vhd
+++ testing/modelsim_work/@i2@c/_primary.vhd
@@ -2,16 +2,15 @@ library verilog;
 use verilog.vl_types.all;
 entity I2C is
     port(
-        Address         : in     vl_logic_vector(6 downto 0);
-        \Register\      : in     vl_logic_vector(7 downto 0);
-        Clk             : in     vl_logic;
-        Mode            : in     vl_logic;
-        Start           : in     vl_logic;
-        Stop            : in     vl_logic;
-        Reset           : in     vl_logic;
-        Ack             : out    vl_logic;
-        \Out\           : out    vl_logic_vector(7 downto 0);
+        address         : in     vl_logic_vector(6 downto 0);
+        \register\      : in     vl_logic_vector(7 downto 0);
+        clk             : in     vl_logic;
+        mode            : in     vl_logic;
+        en              : in     vl_logic;
+        reset           : in     vl_logic;
+        ack             : out    vl_logic;
+        \out\           : out    vl_logic_vector(7 downto 0);
         Sda             : inout  vl_logic;
-        Scl             : inout  vl_logic
+        scl             : inout  vl_logic
     );
 end I2C;
