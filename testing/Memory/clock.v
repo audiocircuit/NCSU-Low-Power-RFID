c06f8dac7ca7a6db242d505070f406dcf721f42d
Joshua Stevens <jasteve4@ncsu.edu>
2014-01-05 15:51:01 -0500

    working on memory control module for I2C communication

--- testing/Memory/clock.v
+++ testing/Memory/clock.v
@@ -1,6 +1,3 @@
-
-`timescale 1ns/1ps
-
 module clock_divider(
   input wire clk,
   input wire reset,
@@ -13,7 +10,7 @@ module clock_divider(
   reg [8:0] counter2;
   reg [8:0] clk_divid;
 
-  always@(posedge clk)
+  always@(posedge clk or negedge reset)
     begin
       if( ~reset )
         begin
@@ -48,33 +45,4 @@ module clock_divider(
             end
         end
     end
-endmodule
-
-module clock_test();
-
-  reg clk;
-  reg reset;
-  reg enabled;
-  wire clk_out;
-  wire clk_offset;
-
-  clock_divider u1 (clk, reset, enabled, clk_out, clk_offset);
-
-  always
-    begin
-      #5 clk <= !clk;
-    end
-
-  initial
-    begin
-      clk = 0;
-      reset = 0;
-      enabled = 0;
-      #20000
-      reset = 1;
-      enabled = 1;
-      #6000000;
-      $finish;
-    end
-
 endmodule
