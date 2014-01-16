073a9841bd8af1581be0dcee46e300b59098917f
Joshua Stevens <jasteve4@ncsu.edu>
2014-01-15 23:11:05 -0500

    Done with FIFO.v and test_FIFO.v

--- FIFO/FIFO.v
+++ FIFO/FIFO.v
@@ -7,64 +7,39 @@ module FIFO(
   input wire write,
   input wire [7:0] data_in,
   output reg [7:0] data_out,
-  output reg empty,
-  output reg full
+  output wire empty,
+  output wire full
   );
 
   reg [3:0] write_pointer;
   reg [3:0] read_pointer;
   reg [7:0] memory [0:7];
   reg [7:0] data;
- 
-  
 
-
-  always@(negedge r_clk or negedge reset_n)
-    begin
-      if( !reset_n || !en  )
-        begin
-          read_pointer <= 0;
-        end
-      else
-        begin
-          if( read && !empty )
-            begin
-              if((read_pointer + 1) < 16)
-                begin
-                  read_pointer <= read_pointer + 1'b1;
-                end
-              else
-                begin
-                  read_pointer <= 0;
-                end
-            end 
-          else
-            begin
-              read_pointer <= read_pointer;
-            end
-        end
-    end
+  assign full = ((read_pointer[3] != write_pointer[3]) && 
+                (read_pointer[2:0] == write_pointer[2:0])) ? 1 : 0; 
+  assign empty = (read_pointer == write_pointer) ? 1 : 0;
 
   always@(posedge w_clk or negedge reset_n)
     begin
-      if( !reset_n || !en  )
+      if( !reset_n )
         begin
           write_pointer <= 0;
-          data <= 0;
+          data <= 8'bz;
         end
       else
         begin
-          if( write && !full )
+          if( write && en && !full )
             begin
-              if((write_pointer + 1'b1) < 16)
+              if((write_pointer +1) < 16)
                 begin
-                  write_pointer <= write_pointer + 1'b1;
+                  write_pointer <= write_pointer + 1;
                   data <= data_in;
                 end
               else
                 begin
                   write_pointer <= 0;
-                  data <= data;
+                  data <= data_in;
                 end
             end
           else
@@ -75,21 +50,20 @@ module FIFO(
         end
     end
 
-  always@( write_pointer or negedge reset_n or read_pointer)
+  always@(write_pointer or negedge reset_n)
     begin
-      if( !reset_n )
+      if(!reset_n)
         begin
-          memory[0] <= 8'b0;
-          memory[1] <= 8'b0;
-          memory[2] <= 8'b0;
-          memory[3] <= 8'b0;
-          memory[4] <= 8'b0;
-          memory[5] <= 8'b0;
-          memory[6] <= 8'b0;
-          memory[7] <= 8'b0;
-          full <= 0;
+          memory[0] <= 0;
+          memory[1] <= 0;
+          memory[2] <= 0;
+          memory[3] <= 0;
+          memory[4] <= 0;
+          memory[5] <= 0;
+          memory[6] <= 0;
+          memory[7] <= 0;
         end
-      else if( !full && write )
+      else
         begin
           case(write_pointer[2:0])
             0:
@@ -101,51 +75,51 @@ module FIFO(
                 memory[4] <= memory[4];
                 memory[5] <= memory[5];
                 memory[6] <= memory[6];
-                memory[7] <= data_in;
+                memory[7] <= data;                 
               end
             1:
               begin
-                memory[0] <= data_in;
+                memory[0] <= data;
                 memory[1] <= memory[1];
                 memory[2] <= memory[2];
                 memory[3] <= memory[3];
                 memory[4] <= memory[4];
                 memory[5] <= memory[5];
                 memory[6] <= memory[6];
-                memory[7] <= memory[7];
+                memory[7] <= memory[7];                 
               end
             2:
               begin
                 memory[0] <= memory[0];
-                memory[1] <= data_in;
+                memory[1] <= data;
                 memory[2] <= memory[2];
                 memory[3] <= memory[3];
                 memory[4] <= memory[4];
                 memory[5] <= memory[5];
                 memory[6] <= memory[6];
-                memory[7] <= memory[7];
+                memory[7] <= memory[7];                 
               end
             3:
               begin
                 memory[0] <= memory[0];
                 memory[1] <= memory[1];
-                memory[2] <= data_in;
+                memory[2] <= data;
                 memory[3] <= memory[3];
                 memory[4] <= memory[4];
                 memory[5] <= memory[5];
                 memory[6] <= memory[6];
-                memory[7] <= memory[7];
+                memory[7] <= memory[7];                 
               end
             4:
               begin
                 memory[0] <= memory[0];
                 memory[1] <= memory[1];
                 memory[2] <= memory[2];
-                memory[3] <= data_in;
+                memory[3] <= data;
                 memory[4] <= memory[4];
                 memory[5] <= memory[5];
                 memory[6] <= memory[6];
-                memory[7] <= memory[7];
+                memory[7] <= memory[7];                 
               end
             5:
               begin
@@ -153,10 +127,10 @@ module FIFO(
                 memory[1] <= memory[1];
                 memory[2] <= memory[2];
                 memory[3] <= memory[3];
-                memory[4] <= data_in;
+                memory[4] <= data;
                 memory[5] <= memory[5];
                 memory[6] <= memory[6];
-                memory[7] <= memory[7];
+                memory[7] <= memory[7];                 
               end
             6:
               begin
@@ -165,9 +139,9 @@ module FIFO(
                 memory[2] <= memory[2];
                 memory[3] <= memory[3];
                 memory[4] <= memory[4];
-                memory[5] <= data_in;
+                memory[5] <= data;
                 memory[6] <= memory[6];
-                memory[7] <= memory[7];
+                memory[7] <= memory[7];                 
               end
             7:
               begin
@@ -177,49 +151,63 @@ module FIFO(
                 memory[3] <= memory[3];
                 memory[4] <= memory[4];
                 memory[5] <= memory[5];
-                memory[6] <= data_in;
-                memory[7] <= memory[7];
+                memory[6] <= data;
+                memory[7] <= memory[7];                 
               end
           endcase
-          full <= ((read_pointer[2:0] == write_pointer[2:0]) &&(read_pointer[3] != write_pointer[3])) ? 1 : 0;
-        end
-      else
-        begin
-          memory[0] <= memory[0];
-          memory[1] <= memory[1];
-          memory[2] <= memory[2];
-          memory[3] <= memory[3];
-          memory[4] <= memory[4];
-          memory[5] <= memory[5];
-          memory[6] <= memory[6];
-          memory[7] <= memory[7];
-          full <= ((read_pointer[2:0] == write_pointer[2:0]) &&(read_pointer[3] != write_pointer[3])) ? 1 : 0;
         end
     end
 
-  always@( read_pointer or negedge reset_n or write_pointer )
+  always@(negedge r_clk or negedge reset_n)
     begin
-      if( !reset_n )
+      if(!reset_n)
+        begin
+          read_pointer <= 0;
+        end
+      else
+        begin
+          if(en)
+            begin
+              if( read && !empty )
+                begin
+                  if((read_pointer + 1) < 16) 
+                    begin
+                      read_pointer <= read_pointer + 1;
+                    end
+                  else
+                    begin
+                      read_pointer <= 0;
+                    end
+                end
+              else
+                begin
+                  read_pointer <= read_pointer;
+                end
+            end
+          else
+            begin
+              read_pointer <= 0;
+            end
+        end
+    end
+
+
+  always@(read_pointer)
+    begin
+      if(!reset_n)
         begin
           data_out <= 8'bz;
-          empty <= 1'b1;
         end
-      else if( read )
+      else
         begin
-          empty <= (read_pointer == write_pointer) ? 1'b1 : 1'b0;
-          if( read_pointer == 0 )
+          if(read_pointer[2:0] == 0)
             begin
               data_out <= memory[7];
             end
           else
             begin
-              data_out <= memory[read_pointer - 1'b1];
+              data_out <= memory[read_pointer[2:0] - 1];
             end
-        end
-      else
-        begin
-          data_out <= 8'bz;
-          empty = (read_pointer == write_pointer) ? 1'b1 : 1'b0;
         end
     end
 endmodule
