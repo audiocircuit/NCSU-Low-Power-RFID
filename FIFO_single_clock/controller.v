d92e05797d555247163ad326608da45e7bc8af4b
Joshua Stevens <jasteve4@ncsu.edu>
2014-03-22 23:02:54 -0400

    Update to FIFO

--- FIFO_single_clock/controller.v
+++ FIFO_single_clock/controller.v
@@ -1,91 +1,3 @@
-/*module FIFO(
-  input wire reset_n,
-  input wire clock,
-  input wire write_en,
-  input wire read_en,
-  input wire [7:0] data_in,
-  output reg [7:0] data_out,
-  input wire en,
-  output wire empty,
-  output wire full
-  );
-  
-  FIFO_controller fifo_controller (
-
-
-  );
-
-  FIFO_Datapath fifo_datapath(
-
-
-  );
-
-
-
-
-endmodule
-*/
-
-module test_controller();
-  reg clock = 1;
-  reg reset_n = 1;
-  reg write_en = 0;
-  reg read_en = 0;
-  wire [1:0] write_pointer;
-  wire [1:0] read_pointer;
-  wire full;
-  wire empty;
-  wire data_ready;
-  wire data_valid;
-
-
-  FIFO_controller u1 (
-    clock,
-    reset_n,
-    write_en,
-    read_en,
-    write_pointer,
-    read_pointer,
-    full,
-    empty,
-    data_ready,
-    data_valid
-    );
-
-
-  always #5 clock <= ~clock;
-
-  initial
-    begin
-      #10
-      reset_n = 0;
-      #10
-      reset_n = 1;
-      #10
-      write_en = 1;
-      #200 // while(!full)
-      #40
-      write_en = 0;
-      #20
-      read_en = 1;
-      #200//while(!empty)
-      #100
-      write_en = 1;
-      #10
-      read_en = 1;
-      #200
-      write_en = 0;
-      #60
-      read_en = 0;
-      #40
-
-      $finish;
-    end
-
-
-endmodule
-
-
 module FIFO_controller(
   input wire clock,
   input wire reset_n,
@@ -166,7 +78,7 @@ module FIFO_controller(
 
   assign write_pointer = {bin_write_pointer[1], bin_write_pointer[1] ^ bin_write_pointer[0]}; 
 
-  always@(posedge clock or negedge reset_n)
+  /*always@(posedge clock or negedge reset_n)
     begin
       if(!reset_n)
         begin
@@ -177,11 +89,12 @@ module FIFO_controller(
           bin_read_pointer <= next_bin_read_pointer;
         end
     end
-
+*/
   always@(posedge clock or negedge reset_n)
     begin
       if(!reset_n)
         begin
+          bin_read_pointer <= 3'b0;
           next_bin_read_pointer <= 3'b0;
           data_valid <= 1'b1;
         end
@@ -189,12 +102,20 @@ module FIFO_controller(
         begin
           if(read_en & !empty & data_valid)
             begin
-              next_bin_read_pointer <= next_bin_read_pointer + 1'b1;
+              bin_read_pointer <= next_bin_read_pointer;
+              next_bin_read_pointer <= bin_read_pointer;
               data_valid <= 1'b0;
+            end
+          else if(read_en & !empty & !data_valid)
+            begin
+              next_bin_read_pointer <= bin_read_pointer + 1'b1;
+              bin_read_pointer <= bin_read_pointer;
+              data_valid <= 1'b1;
             end
           else
             begin
               next_bin_read_pointer <= next_bin_read_pointer;
+              bin_read_pointer <= next_bin_read_pointer;
               data_valid <= 1'b1;
             end
         end
@@ -206,66 +127,3 @@ module FIFO_controller(
 endmodule
 
 
-module FIFO_Datapath(
-  input wire reset_n,
-  input wire data_in,
-  output reg data_out,
-  input wire read_pointer,
-  input wire write_pointer,
-  input wire data_ready,
-  input wire data_valid
-  );
-  integer i;
-  reg [7:0] memory [0:3];
-  
-  always@(posedge data_ready or negedge reset_n)
-    begin
-      if(!reset_n)
-        begin
-          for(i = 5'b0; i < 5'd4; i = i + 1'b1)
-            begin
-              memory[i] <= 5'b0;
-            end
-        end
-      else
-        begin
-          for(i = 5'b0; i < 5'd4; i = i + 1'b1)
-            begin
-              if(i == write_pointer)
-                begin
-                  memory[i] <= data_in;
-                end
-              else
-                begin
-                  memory[i] <= memory[i];
-                end
-            end
-        end
-    end
-
-  always@(posedge data_valid or negedge reset_n)
-    begin
-      if(!reset_n)
-        begin
-          for(i = 5'b0; i < 5'd4; i = i + 1'b1)
-            begin
-              data_out <= 8'b0;
-            end
-        end
-      else
-        begin
-          for(i = 5'b0; i < 5'd4; i = i + 1'b1)
-            begin
-              if(i == read_pointer)
-                begin
-                  data_out <= memory[i];
-                end
-              else
-                begin
-                  data_out <= data_out;
-                end
-            end
-        end
-    end
-
-endmodule
