92e90d501b0661ccff1b6af00749d722fd2425d5
audiocircuit <scott891@gmail.com>
2014-04-17 23:10:31 -0400

    now accepts data from user memory correctly, still has problems with I2C

--- System_Controller/controller.v
+++ System_Controller/controller.v
@@ -9,11 +9,6 @@ DESCRIPTION: This module controls the state of the overall RFID Chip.
 module controller(
   input clock,
   input rst_n,
-  //decode_OP inputs and outputs
-  input [1:0]sensor_num,
-  input [2:0]op_code,
-  input [15:0]data_Decode,
-  input dataRdy_Decode,
   //Wr_Sensor inputs and outputs
   input [7:0]readVal_I2C,
   input dataRdy_I2C,
@@ -21,16 +16,12 @@ module controller(
   output reg [7:0]writeVal_I2C,
   output reg mode_I2C,
   output reg start_I2C,
-  //User Memory inputs and outputs
-  output reg we_userMem,
-  output reg [15:0] writeAddr_userMem,
-  output reg [15:0] writeBus_userMem,
   //EEPROM inputs and outputs
-  input [15:0] readBus_EEPROM,
-  output reg we_EEPROM,
-  output reg [15:0]writeAddr_EEPROM,
-  output reg [15:0]writeBus_EEPROM,
-  output reg [15:0]readAddr_EEPROM
+  input [15:0] readBus_userMem,
+  output reg we_userMem,
+  output reg [14:0]writeAddr_userMem,
+  output reg [15:0]writeBus_userMem,
+  output reg [14:0]readAddr_userMem
 );
 
 //parameters for state machine
@@ -49,129 +40,182 @@ parameter INCREMENT_SENSOR      = 9;
 parameter I2C_WRITE             = 1'b0;
 parameter I2C_READ              = 1'b1;
 
+reg test;
+
 //define reg/wires
-reg [5:0] cstate, nstate;
+reg [5:0] state;
 reg [15:0] sensorInfo [0:3];
 reg [1:0]readSensor;
 
+//State machine control logic
 always@(posedge clock or negedge rst_n) begin
   if(!rst_n) begin
-    cstate <= RESET;
+    state <= RESET;
   end else begin
-    cstate <= nstate;
+    case(state)
+      RESET: begin
+        readSensor <= 2'b0;  
+        sensorInfo[0] <= 16'b0;
+        sensorInfo[1] <= 16'b0;
+        sensorInfo[2] <= 16'b0;
+        sensorInfo[3] <= 16'b0;
+        state <= GET_SENSOR0_INFO; 
+
+        test <= 0;
+      end
+
+      GET_SENSOR0_INFO: begin
+        readSensor <= 2'b0;
+        sensorInfo[0] <= readBus_userMem;
+        sensorInfo[1] <= 16'b0;
+        sensorInfo[2] <= 16'b0;
+        sensorInfo[3] <= 16'b0;
+        state <= GET_SENSOR1_INFO;
+        
+        test <= ~test;
+      end
+
+      GET_SENSOR1_INFO: begin
+        readSensor <= 2'b0;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= readBus_userMem;
+        sensorInfo[2] <= 16'b0;
+        sensorInfo[3] <= 16'b0;
+        state <= GET_SENSOR2_INFO;
+
+        test <= ~test;
+      end
+
+      GET_SENSOR2_INFO: begin
+        readSensor <= 2'b0;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= sensorInfo[1];
+        sensorInfo[2] <= readBus_userMem;
+        sensorInfo[3] <= 16'b0;
+        state <= GET_SENSOR3_INFO;
+      end
+
+      GET_SENSOR3_INFO: begin
+        readSensor <= 2'b0;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= sensorInfo[1];
+        sensorInfo[2] <= sensorInfo[2];
+        sensorInfo[3] <= readBus_userMem;
+        state <= WRITE_SENSOR_CB_READ;
+      end
+
+      WRITE_SENSOR_CB_READ: begin
+        readSensor <= readSensor + 1'b0;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= sensorInfo[1];
+        sensorInfo[2] <= sensorInfo[2];
+        sensorInfo[3] <= sensorInfo[3];
+        if(dataRdy_I2C) begin
+          state <= READ_SENSOR_VAL;
+        end else begin
+          state <= WRITE_SENSOR_CB_READ;
+        end
+      end
+
+      READ_SENSOR_VAL: begin
+        readSensor <= readSensor + 1'b0;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= sensorInfo[1];
+        sensorInfo[2] <= sensorInfo[2];
+        sensorInfo[3] <= sensorInfo[3];
+        if(dataRdy_I2C) begin
+          state <= LOOKUP_SENSOR_VAL;
+        end else begin
+          state <= READ_SENSOR_VAL;
+        end
+      end
+
+      LOOKUP_SENSOR_VAL: begin
+        readSensor <= readSensor + 1'b0;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= sensorInfo[1];
+        sensorInfo[2] <= sensorInfo[2];
+        sensorInfo[3] <= sensorInfo[3];
+        state <= WRITE_TO_USERMEM;
+      end
+
+      WRITE_TO_USERMEM: begin
+        readSensor <= readSensor + 1'b0;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= sensorInfo[1];
+        sensorInfo[2] <= sensorInfo[2];
+        sensorInfo[3] <= sensorInfo[3];
+        state <= INCREMENT_SENSOR;
+      end
+
+      INCREMENT_SENSOR: begin
+        readSensor <= readSensor + 1'b1;
+        sensorInfo[0] <= sensorInfo[0];
+        sensorInfo[1] <= sensorInfo[1];
+        sensorInfo[2] <= sensorInfo[2];
+        sensorInfo[3] <= sensorInfo[3];
+        state <= WRITE_SENSOR_CB_READ;
+      end
+    endcase
   end
 end
 
+
 always@(*) begin
-  case(cstate)
+  case(state)
     RESET: begin
       sensorAddr_I2C <= 7'b0;
       writeVal_I2C <= 8'b0;
       mode_I2C <= 1'b0;
       start_I2C <= 1'b0;
       we_userMem <= 1'b0;
-      writeAddr_userMem <= 16'b0;
+      writeAddr_userMem <= 15'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= 16'b1_000_000000000000;
-      sensorInfo[0] <= 16'b0;
-      sensorInfo[1] <= 16'b0;
-      sensorInfo[2] <= 16'b0;
-      sensorInfo[3] <= 16'b0;
-      readSensor <= 2'b0;
-      
-      nstate <= GET_SENSOR0_INFO;
+      readAddr_userMem <= 15'b100_0000_0000_0000;
     end
-    
-    //Get Sensor0 Address, I2C Control Byte, and Default Sensor
+
     GET_SENSOR0_INFO: begin
       sensorAddr_I2C <= 7'b0;
       writeVal_I2C <= 8'b0;
       mode_I2C <= 1'b0;
       start_I2C <= 1'b0;
       we_userMem <= 1'b0;
-      writeAddr_userMem <= 16'b0;
+      writeAddr_userMem <= 15'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= 16'b1_001_000000000000;
-      sensorInfo[0] <= readBus_EEPROM;
-      sensorInfo[1] <= 16'b0;
-      sensorInfo[2] <= 16'b0;
-      sensorInfo[3] <= 16'b0;
-      readSensor <= readSensor;
-
-      nstate <= GET_SENSOR1_INFO;
+      readAddr_userMem <= 15'b100_0000_0000_0000;
     end
 
-    //Get Sensor1 Address, I2C Control Byte, and Default Sensor
     GET_SENSOR1_INFO: begin
       sensorAddr_I2C <= 7'b0;
       writeVal_I2C <= 8'b0;
       mode_I2C <= 1'b0;
       start_I2C <= 1'b0;
       we_userMem <= 1'b0;
-      writeAddr_userMem <= 16'b0;
+      writeAddr_userMem <= 15'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= 16'b1_010_000000000000;
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= readBus_EEPROM;
-      sensorInfo[2] <= 16'b0;
-      sensorInfo[3] <= 16'b0;
-      readSensor <= readSensor;
-
-      nstate <= GET_SENSOR2_INFO;
+      readAddr_userMem <= 15'b100_0000_0001_0000;
     end
-    
-    //Get Sensor2 Address, I2C Control Byte, and Default Sensor
+
     GET_SENSOR2_INFO: begin
       sensorAddr_I2C <= 7'b0;
       writeVal_I2C <= 8'b0;
       mode_I2C <= 1'b0;
       start_I2C <= 1'b0;
       we_userMem <= 1'b0;
-      writeAddr_userMem <= 16'b0;
+      writeAddr_userMem <= 15'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= 16'b1_011_000000000000;
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= sensorInfo[1];
-      sensorInfo[2] <= readBus_EEPROM;
-      sensorInfo[3] <= 16'b0;
-      readSensor <= readSensor;
-
-      nstate <= GET_SENSOR3_INFO;
-
+      readAddr_userMem <= 15'b100_0000_0010_0000;
     end
 
-    //Get Sensor3 Address, I2C Control Byte, and Default Sensor
     GET_SENSOR3_INFO: begin
       sensorAddr_I2C <= 7'b0;
       writeVal_I2C <= 8'b0;
       mode_I2C <= 1'b0;
       start_I2C <= 1'b0;
       we_userMem <= 1'b0;
-      writeAddr_userMem <= 16'b0;
+      writeAddr_userMem <= 15'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= 16'b1_011_000000000000;
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= sensorInfo[1];
-      sensorInfo[2] <= sensorInfo[2];
-      sensorInfo[3] <= readBus_EEPROM; 
-      readSensor <= readSensor;
-
-      nstate <= WRITE_SENSOR_CB_READ;
+      readAddr_userMem <= 15'b100_0000_0011_0000;
     end
 
     WRITE_SENSOR_CB_READ: begin
@@ -182,17 +226,7 @@ always@(*) begin
       we_userMem <= 1'b0;
       writeAddr_userMem <= 16'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= 16'b0;
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= sensorInfo[1];
-      sensorInfo[2] <= sensorInfo[2];
-      sensorInfo[3] <= sensorInfo[3];
-      readSensor <= readSensor;
-
-      nstate <= READ_SENSOR_VAL;
+      readAddr_userMem <= 16'b0;
     end
 
     READ_SENSOR_VAL: begin
@@ -203,17 +237,7 @@ always@(*) begin
       we_userMem <= 1'b0;
       writeAddr_userMem <= 16'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= 16'b0;
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= sensorInfo[1];
-      sensorInfo[2] <= sensorInfo[2];
-      sensorInfo[3] <= sensorInfo[3];
-      readSensor <= readSensor;
-
-      nstate <= LOOKUP_SENSOR_VAL;
+      readAddr_userMem <= 16'b0;
     end
 
     LOOKUP_SENSOR_VAL: begin
@@ -224,17 +248,7 @@ always@(*) begin
       we_userMem <= 1'b0;
       writeAddr_userMem <= 16'b0;
       writeBus_userMem <= 16'b0;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= {6'b0,readSensor,readVal_I2C};
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= sensorInfo[1];
-      sensorInfo[2] <= sensorInfo[2];
-      sensorInfo[3] <= sensorInfo[3];
-      readSensor <= readSensor;
-
-      nstate <= WRITE_TO_USERMEM;
+      readAddr_userMem <= {1'b0,readSensor,4'b0,readVal_I2C};
     end
 
     WRITE_TO_USERMEM: begin
@@ -243,19 +257,9 @@ always@(*) begin
       mode_I2C <= 1'b0;
       start_I2C <= 1'b0;
       we_userMem <= 1'b1;
-      writeAddr_userMem <= readSensor;
-      writeBus_userMem <= readBus_EEPROM[15:0];
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= readAddr_EEPROM;
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= sensorInfo[1];
-      sensorInfo[2] <= sensorInfo[2];
-      sensorInfo[3] <= sensorInfo[3];
-      readSensor <= readSensor;
-
-      nstate <= INCREMENT_SENSOR;
+      writeAddr_userMem <= writeAddr_userMem;
+      writeBus_userMem <= readBus_userMem;
+      readAddr_userMem <= 15'b0;
     end
 
     INCREMENT_SENSOR: begin
@@ -266,17 +270,7 @@ always@(*) begin
       we_userMem <= 1'b1;
       writeAddr_userMem <= writeAddr_userMem;
       writeBus_userMem <= writeBus_userMem;
-      we_EEPROM <= 1'b0;
-      writeAddr_EEPROM <= 16'b0;
-      writeBus_EEPROM <= 16'b0;
-      readAddr_EEPROM <= readAddr_EEPROM;
-      sensorInfo[0] <= sensorInfo[0];
-      sensorInfo[1] <= sensorInfo[1];
-      sensorInfo[2] <= sensorInfo[2];
-      sensorInfo[3] <= sensorInfo[3];
-      readSensor <= readSensor + 1'b1;
-
-      nstate <= WRITE_SENSOR_CB_READ;
+      readAddr_userMem <= 15'b0;
     end
   endcase
 end
